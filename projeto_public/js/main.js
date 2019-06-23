31ead960710133446778b2b55087638bf3f872c2
Darlan Nascimento <darlan.nascimento16@gmail.com>
2019-06-23 17:18:05 -0300

    Criado diretório desenvolvimento responsivo e arquivos

---
+++ projeto_public/js/main.js
@@ -0,0 +1,44 @@
+//JavaScript Puro
+//let botao = document.querySelector('.menu-principal__btn');
+//let menuPrincipal = document.querySelector('.menu-principal');
+
+//botao.addEventListener("click",abreFechaMenu);
+
+//function abreFechaMenu(evento){
+//    menuPrincipal.classList.toggle('menu-principal--fechado');
+//}
+
+//jquery
+$(document).ready(function(){
+    //botao do menu
+    $('.menu-principal__btn').click(function(){
+        $('.menu-principal').toggleClass('menu-principal--fechado')
+    });
+
+    // Slide depoimentos
+    $('.depoimentos__caixa').slick({
+        autoplay: true,
+        arrows: false,
+        dots: true,
+    });
+
+    // validação do formulario de contato
+     $('.formulario').validate({
+         rules:{
+             nome: 'required',
+             email:{
+                 required: true,
+                 email: true
+             },
+             mensagem: 'required'
+         },
+         messages:{
+             nome: "Por favor preencha o campo Nome",
+             email:{
+                 required: 'Por favor preencha o e-mail.',
+                 email: 'Por favor preencha um e-mail válido'
+             },
+             mensagem: "Por favor preencha o campo Mensagem"
+         }
+     });
+});
---
+++ projeto_public/partes/rodape.php
@@ -0,0 +1,40 @@
+</Main>
+    <footer class="rodape">
+        <div class="rodape__linha-1">
+            <div class="container">
+                <div class="rodape__coluna">
+                    <a href="index.html" class="logo logo--branco">Design Responsivo</a>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam necessitatibus expedita tenetur quia iste incidunt dicta, in consequuntur fugit, et, nihil velit vel illo itaque! Reiciendis quas dignissimos incidunt illum.</p>
+                </div>
+                <div class="rodape__coluna">
+                    <h2 class="rodape__titulo">Social</h2>
+                    <ul class="rodape__lista-links">
+                        <li class="rodape__lista-links__item"><a href="https://twitter.com" target="_blank">Twitter</a></li>
+                        <li class="rodape__lista-links__item"><a href="https://www.facebook.com" target="_blank">Facebook</a></li>
+                        <li class="rodape__lista-links__item"><a href="https://www.instagram.com" target="_blank">Instagram</a></li>
+                        <li class="rodape__lista-links__item"><a href="https://www.youtube.com" target="_blank">YouTube</a></li>
+                    </ul>
+                </div>
+                <div class="rodape__coluna">
+                    <h2 class="rodape__titulo">Links</h2>
+                    <ul class="rodape__lista-links">
+                            <li class="rodape__lista-links__item"><a href="index.php">Home</a></li>
+                            <li class="rodape__lista-links__item"><a href="sobre.php">Sobre nós</a></li>
+                            <li class="rodape__lista-links__item"><a href="portfolio.php">Portfolio</a></li>
+                            <li class="rodape__lista-links__item"><a href="contato.php">Contato</a></li>
+                        </ul>
+                </div>
+            </div><!--fim container-->
+        </div>
+        <div class="rodape__linha-2">
+            © <?php echo date('Y')?> - Todos os direitos reservados - Darlan Nascimento
+        </div>
+    </footer>
+    <!-- <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/slick.min.js"></script>
+    <script src="js/jquery.validate.min.js"></script>
+    <script src="js/jquery.fancybox.min.js"></script>
+    <script src="js/main.js"></script> -->
+    <script src="js/app.min.js"></script>
+</body>
+</html>
